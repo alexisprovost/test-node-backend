504d7e9a0180bdc7fed885db8e2176367b7fc0f2
Alexis Provost <provost21j@gmail.com>
2023-04-06 21:12:11 +0000

    added env for port

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
